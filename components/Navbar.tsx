5bf7ca4a5420f58064aa4b27bf1d48ef6aca4cf7
KbraEser <kbra.demirbas@gmail.comm>
2026-01-11 13:16:04 +0300

    Update dependencies and enhance layout with new components. Added Radix UI components, Tailwind CSS utilities, and improved global styles. Removed unused SVG files and refactored the homepage layout.

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { Moon } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* LEFT */}
+      collapseButton
+      {/* RIGHT */}
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+        <Moon />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
